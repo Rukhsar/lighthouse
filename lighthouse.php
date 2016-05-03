721c32aac0474d96a43162a0f95272d46ff7a3d0
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-05-03 11:40:30 +0100

    lighthouse class with variables

--- lighthouse.php
+++ lighthouse.php
@@ -8,7 +8,41 @@
  * Released under the MIT license
  */
 
+class lighthouse {
 
+    // General
+    protected $database_type;
+
+    protected $charset;
+
+    protected $database_name;
+
+    // For MySQL, MariaDB, MSSQL, Sybase, PostgreSQL, Oracle
+    protected $server;
+
+    protected $username;
+
+    protected $password;
+
+    // For SQLite
+    protected $database_file;
+
+    // For MySQL or MariaDB with unix_socket
+    protected $socket;
+
+    // Optional
+    protected $port;
+
+    protected $prefix;
+
+    protected $option = array();
+
+    // Variable
+    protected $logs = array();
+
+    protected $debug_mode = false;
+
+}
 
 
 ?>

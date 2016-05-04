a8098c3d6fb6aa3ad493367e35653431241a4783
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-05-04 21:55:24 +0100

    __construct

--- lighthouse.php
+++ lighthouse.php
@@ -8,7 +8,7 @@
  * Released under the MIT license
  */
 
-class lighthouse {
+class LightHouse {
 
     // General
     protected $database_type;
@@ -42,6 +42,115 @@ class lighthouse {
 
     protected $debug_mode = false;
 
+    public function __construct($options = null)
+    {
+        try {
+            $commands = array();
+            $dsn = '';
+
+            if (is_array($options))
+            {
+                foreach ($options as $option => $value)
+                {
+                    $this->$option = $value;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            if (
+                isset($this->port) &&
+                is_int($this->port * 1)
+            )
+            {
+                $port = $this->port;
+            }
+
+            $type = strtolower($this->database_type);
+            $is_port = isset($port);
+
+            if (isset($options[ 'prefix' ]))
+            {
+                $this->prefix = $options[ 'prefix' ];
+            }
+
+            switch ($type)
+            {
+                case 'mariadb':
+                    $type = 'mysql';
+
+                case 'mysql':
+                    if ($this->socket)
+                    {
+                        $dsn = $type . ':unix_socket=' . $this->socket . ';dbname=' . $this->database_name;
+                    }
+                    else
+                    {
+                        $dsn = $type . ':host=' . $this->server . ($is_port ? ';port=' . $port : '') . ';dbname=' . $this->database_name;
+                    }
+
+                    // Make MySQL using standard quoted identifier
+                    $commands[] = 'SET SQL_MODE=ANSI_QUOTES';
+                    break;
+
+                case 'pgsql':
+                    $dsn = $type . ':host=' . $this->server . ($is_port ? ';port=' . $port : '') . ';dbname=' . $this->database_name;
+                    break;
+
+                case 'sybase':
+                    $dsn = 'dblib:host=' . $this->server . ($is_port ? ':' . $port : '') . ';dbname=' . $this->database_name;
+                    break;
+
+                case 'oracle':
+                    $dbname = $this->server ?
+                        '//' . $this->server . ($is_port ? ':' . $port : ':1521') . '/' . $this->database_name :
+                        $this->database_name;
+
+                    $dsn = 'oci:dbname=' . $dbname . ($this->charset ? ';charset=' . $this->charset : '');
+                    break;
+
+                case 'mssql':
+                    $dsn = strstr(PHP_OS, 'WIN') ?
+                        'sqlsrv:server=' . $this->server . ($is_port ? ',' . $port : '') . ';database=' . $this->database_name :
+                        'dblib:host=' . $this->server . ($is_port ? ':' . $port : '') . ';dbname=' . $this->database_name;
+
+                    // Keep MSSQL QUOTED_IDENTIFIER is ON for standard quoting
+                    $commands[] = 'SET QUOTED_IDENTIFIER ON';
+                    break;
+
+                case 'sqlite':
+                    $dsn = $type . ':' . $this->database_file;
+                    $this->username = null;
+                    $this->password = null;
+                    break;
+            }
+
+            if (
+                in_array($type, explode(' ', 'mariadb mysql pgsql sybase mssql')) &&
+                $this->charset
+            )
+            {
+                $commands[] = "SET NAMES '" . $this->charset . "'";
+            }
+
+            $this->pdo = new PDO(
+                $dsn,
+                $this->username,
+                $this->password,
+                $this->option
+            );
+
+            foreach ($commands as $value)
+            {
+                $this->pdo->exec($value);
+            }
+        }
+        catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
 }
 
 
